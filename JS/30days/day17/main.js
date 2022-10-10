be1d2245698ed08beeff67dcba28d2569c7def09
mikyYun <mkyun2714@gmail.com>
2022-10-10 11:50:14 -0700

    day17/30

---
+++ JS/30days/day17/main.js
@@ -0,0 +1,22 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+// find "a" or "the" or "an" and remove. then trim..
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an)/i, "").trim();
+}
+
+// const sortedBands = bands.sort((a, b) => {
+//   const stripA = strip(a)
+//   const stripB = strip(b)
+//   return stripA > stripB ? 1 : -1;
+// });
+const sortedBands = bands.sort((a, b) => {
+  strip(a) > strip(b) ? 1 : -1
+}
+);
+
+document.querySelector("#bands").innerHTML = sortedBands.map(band => `
+  <li>
+    ${band}
+  </li>
+`).join("")
